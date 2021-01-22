45407a46d8d4bc32ab0090e8383d68c050ace036
krbtechnology <77837560+krbtechnology@users.noreply.github.com>
2021-01-22 17:27:29 +0530

    added files

---
+++ 1.js
@@ -0,0 +1,15 @@
+
+    var weight=parseFloat(prompt("Enter a Value"));
+
+    switch(weight)
+    {
+        case 1:document.write("weight is 1");
+        break;
+        case 2:document.write("weight is 2");
+        break;
+        default:document.write("Weight "+weight+" is the value");
+
+
+    }
+        
+    
